ec9da927622c4cb20ba6c7664b209f19b7185849
Simon Andersen <simo414u@stud.kea.dk>
2021-03-24 16:25:01 +0100

    Forgot to add all changes. NOW it is working

--- toggle.js
+++ toggle.js
@@ -1,109 +1,70 @@
 "use strict";
 
-//object for all features
-const parts = {
-  heads: {
-    head_1: false,
-    head_2: false,
-    head_3: false,
-    head_4: false,
-  },
-  bodies: {
-    body_2: false,
-    body_3: false,
-  },
+import { startManipulatingSvg } from "./color.js";
 
-  //add r/l arm
-  arms: {
-    arm_1: false,
-    arm_2: false,
-    arm_3: false,
-  },
-  legs: {
-    legs_1: false,
-    legs_2: false,
-    legs_3: false,
-  },
-};
-
-//rewrite for if more heads are true and put in separate function, it is 0 now
-// let headsTrue = Object.values(heads).filter((heads) => heads === true).length;
-// console.log(headsTrue);
 
 window.addEventListener("DOMContentLoaded", init);
 
-function init() {
-  console.log("init");
-  //select all body parts and for each part add toggle
-  document
-    .querySelectorAll("#bodyparts")
-    .forEach((part) => part.addEventListener("click", togglePart));
-  document.querySelector("#btn button").addEventListener("click", resetRobot);
+//object for all features
+let parts = { // Changed to let, as I couldn't change the values on the const (even if we should be able to?)
+    head: "",
+    body: "",
+    arm_right: "",
+    arm_left: "",
+    legs: "",
+};
+
+async function init() {
+    let response = await fetch("robot/robot_parts.svg");
+    let svgData = await response.text();
+    document.querySelector('#preview').innerHTML = svgData;
+    console.log
+    makeClickableParts();
+    startManipulatingSvg();
 }
 
-//REMOVE FROM PREVIEW AND REMOVE ACTIVE CLASS
-function resetRobot() {
-  document
-    .querySelector("#btn button")
-    .removeEventListener("click", resetRobot);
-  const previewImg = document.querySelectorAll("[data-name]");
-  previewImg.forEach((img) => img.classList.add("hide"));
-
-  const partsImg = document.querySelectorAll("[data-part]");
-  partsImg.forEach((img) => img.parentElement.classList.remove("active"));
+function makeClickableParts() {
+  //select all body parts and for each part add toggle #bodyparts
+  document.querySelectorAll(".part_container").forEach(part =>
+    part.addEventListener("click", togglePart)
+  );
 }
 
-//TOGLE BODY PARTS
-function togglePart(e) {
+function togglePart(e) { // This code can be used for any part, heads, arms, legs, bodies etc.
   const target = e.target;
-  console.log(target);
-  //all parts img
-  const bodyPartImg = target.dataset.part;
-  //all preview img
-  const previewImg = document.querySelector(`[data-name = ${bodyPartImg}]`);
-  console.log(bodyPartImg);
-  console.log(previewImg);
+  console.log(target)
+  // This gets the img inside the .part_container. Also see preview.css. I added pointer-events: none to the img, so you can only click the container:
+  const bodyPartImg = target.children[0]; // This will give us the img element   
+  console.log(bodyPartImg)      
+  const clickedPart = bodyPartImg.getAttribute('data-part'); // This will get us the identifier (the data-part) we want
+  const partName = clickedPart.slice(0, clickedPart.lastIndexOf('_')); // Gets part name without number ('_2')
+  // console.log(clickedPart);
+  // console.log(partName);
+  removeOldPart();
 
-  //make IF statements for toggling parts, and if > 2 remove first ****
-  //CHECKS IF CHOSEN IS > , but AFFECTS ALL
-  let partsTrue = Object.values(heads).filter((head) => head === true).length;
-  console.log(partsTrue);
+  function removeOldPart() {
+    const partToHide = parts[partName];
+    console.log(partToHide);
 
-  //all body parts
-  if (heads[bodyPartImg] === false) {
-    heads[bodyPartImg] = true;
-
-    //ADD ACTIVE CLASS
-    target.parentElement.classList.add("active");
-    //UNHIDE BODY PART
-    previewImg.classList.remove("hide");
-
-    // //Call new featureElement function to create element and add it to the list
-    console.log(target);
-    let parent = document.querySelector("#chosen ul");
-    let childElement = document.createElement("li");
-    console.log(parent);
-    let childImg = target.cloneNode(true);
-    childElement.appendChild(childImg);
-    parent.appendChild(childElement);
-
-
-    //MAKE FLIP ANIMATION
-  } else {
-    heads[bodyPartImg] = false;
-    target.parentElement.classList.remove("active");
-    document
-      .querySelector(`[data-name="${bodyPartImg}"]`)
-      .classList.add("hide");
+    if (partToHide === "") { // If nothing was previously selected, nothing will be removed
+      console.log('Nothing to be removed as nothing was previously selected!');
+      addNewPart();
+    } else if (partToHide === clickedPart) { // If part already selected, hide it again
+      console.log('already selected this')
+      parts[partName] = ""; // Remove clicked part from parts obj again
+      document.querySelector(`#${partToHide}`).classList.add("hide");
+    } else {
+      document.querySelector(`#${partToHide}`).classList.add("hide");
+      addNewPart();
+    }
   }
+  
+  function addNewPart() {
+    parts[partName] = clickedPart; // Update parts obj with the just selected part
+    //console.log(parts);
+    const partToShow = document.querySelector(`#${clickedPart}`);
+    console.log(partToShow);
+    partToShow.classList.remove("hide");
+  }
+  
 }
-
-// function createChosenElement(chosenImg) {
-//   const li = document.createElement("li");
-//   li.dataset.name = chosenImg;
-//   console.log(chosenImg.dataset.name);
-//   const img = document.createElement("img");
-//   img.src = `./robots/${chosenImg.dataset.name}.png`;
-//   li.append(img);
-//   return li;
-// }
