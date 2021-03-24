32639cd63bcacd7609c0679bf26897661fea6388
Simon Andersen <simo414u@stud.kea.dk>
2021-03-24 21:01:47 +0100

    Fill set to none when part is deselected

--- toggle.js
+++ toggle.js
@@ -32,10 +32,10 @@ function makeClickableParts() {
 
 function togglePart(e) { // This code can be used for any part, heads, arms, legs, bodies etc.
   const target = e.target;
-  console.log(target)
+  // console.log(target)
   // This gets the img inside the .part_container. Also see preview.css. I added pointer-events: none to the img, so you can only click the container:
   const bodyPartImg = target.children[0]; // This will give us the img element   
-  console.log(bodyPartImg)      
+  // console.log(bodyPartImg)      
   const clickedPart = bodyPartImg.getAttribute('data-part'); // This will get us the identifier (the data-part) we want
   const partName = clickedPart.slice(0, clickedPart.lastIndexOf('_')); // Gets part name without number ('_2')
   // console.log(clickedPart);
@@ -44,7 +44,7 @@ function togglePart(e) { // This code can be used for any part, heads, arms, leg
 
   function removeOldPart() {
     const partToHide = parts[partName];
-    console.log(partToHide);
+    // console.log(partToHide);
 
     if (partToHide === "") { // If nothing was previously selected, nothing will be removed
       console.log('Nothing to be removed as nothing was previously selected!');
@@ -53,6 +53,10 @@ function togglePart(e) { // This code can be used for any part, heads, arms, leg
       console.log('already selected this')
       parts[partName] = ""; // Remove clicked part from parts obj again
       document.querySelector(`#${partToHide}`).classList.add("hide");
+      const colorElements = document.querySelectorAll(`#${partToHide} .colorize`); // Get the g elements inside the part
+      console.log(colorElements);
+      colorElements.forEach(elem => 
+        elem.style.fill = "none" );
     } else {
       document.querySelector(`#${partToHide}`).classList.add("hide");
       addNewPart();
@@ -63,8 +67,77 @@ function togglePart(e) { // This code can be used for any part, heads, arms, leg
     parts[partName] = clickedPart; // Update parts obj with the just selected part
     //console.log(parts);
     const partToShow = document.querySelector(`#${clickedPart}`);
-    console.log(partToShow);
+    // console.log(partToShow);
     partToShow.classList.remove("hide");
   }
   
 }
+
+// -------------------------------------------------------------------------- old code --->
+//REMOVE FROM PREVIEW AND REMOVE ACTIVE CLASS
+// function resetRobot() {
+//   document
+//     .querySelector("#btn button")
+//     .removeEventListener("click", resetRobot);
+//   const previewImg = document.querySelectorAll("[data-name]");
+//   previewImg.forEach((img) => img.classList.add("hide"));
+
+//   const partsImg = document.querySelectorAll("[data-part]");
+//   partsImg.forEach((img) => img.parentElement.classList.remove("active"));
+// }
+
+// //TOGLE BODY PARTS
+// function togglePart(e) {
+//   const target = e.target;
+//   console.log(target);
+//   //all parts img
+//   const bodyPartImg = target.dataset.part;
+//   //all preview img
+//   const previewImg = document.querySelector(`[data-name = ${bodyPartImg}]`);
+//   console.log(bodyPartImg);
+//   console.log(previewImg);
+
+//   //make IF statements for toggling parts, and if > 2 remove first ****
+//   //CHECKS IF CHOSEN IS > , but AFFECTS ALL
+//   let partsTrue = Object.values(heads).filter((head) => head === true).length;
+//   console.log(partsTrue);
+
+//   //all body parts
+//   if (heads[bodyPartImg] === false) {
+//     heads[bodyPartImg] = true;
+
+//     //ADD ACTIVE CLASS
+//     target.parentElement.classList.add("active");
+//     //UNHIDE BODY PART
+//     previewImg.classList.remove("hide");
+
+//     // //Call new featureElement function to create element and add it to the list
+//     console.log(target);
+//     let parent = document.querySelector("#chosen ul");
+//     let childElement = document.createElement("li");
+//     console.log(parent);
+//     let childImg = target.cloneNode(true);
+//     childElement.appendChild(childImg);
+//     parent.appendChild(childElement);
+
+
+//     //MAKE FLIP ANIMATION
+//   } else {
+//     heads[bodyPartImg] = false;
+//     target.parentElement.classList.remove("active");
+//     document
+//       .querySelector(`[data-name="${bodyPartImg}"]`)
+//       .classList.add("hide");
+//   }
+// }
+
+// function createChosenElement(chosenImg) {
+//   const li = document.createElement("li");
+//   li.dataset.name = chosenImg;
+//   console.log(chosenImg.dataset.name);
+//   const img = document.createElement("img");
+//   img.src = `./robots/${chosenImg.dataset.name}.png`;
+//   li.append(img);
+//   return li;
+// }
+
